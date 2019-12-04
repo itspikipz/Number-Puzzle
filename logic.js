1f671c0132248d5c4eadd2ab0001bc256fbd8715
itspikipz <58529620+itspikipz@users.noreply.github.com>
2019-12-04 21:38:59 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -106,7 +106,6 @@ var board=[["1.jpg data-id = '1'", "2.jpg data-id = '2'", "3.jpg data-id = '3'"]
 var levizjet = 0;
 var rekordi = 9999;
 function play(){
-  $(".piket").html("<p>Levizjet</p><p>"+0+"</p>");
   $(".trupi").removeClass("mainanm");
   $(".main").removeClass("borderWhite");
   $(".piket").removeClass("afterWinPiketRekordi");
